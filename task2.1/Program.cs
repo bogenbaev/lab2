394979c6aaf50f59755e575ed1845490f5646ed0
bogenbaev <qwerty_lol01@mail.ru>
2019-02-03 18:46:07 +0600

    task2.1

---
+++ task2.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2._1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            bool k = true;
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+            {
+                if (s[i] != s[j])
+                {
+                    k = false;
+                    break;
+                }
+                k = true;
+            }
+            if (k == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
